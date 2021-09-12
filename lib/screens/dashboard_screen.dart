78da775089ad35301d2fb07a4cdbd22d41227b10
SamElgohary <eslam.elgohary902@gmail.com>
2021-09-12 02:13:14 +0200

    home 2

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,17 +1,9 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:flutter_login/theme.dart';
-import 'package:flutter_login/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:ui_app_saif/widgets/animated_numeric_text.dart';
 import 'package:ui_app_saif/widgets/color_resources.dart';
 import 'package:ui_app_saif/widgets/fade_in.dart';
-import 'package:ui_app_saif/widgets/round_button.dart';
-
-import '../constants.dart';
 import '../transition_route_observer.dart';
-
 
 class DashboardScreen extends StatefulWidget {
   static const routeName = '/dashboard';
@@ -33,6 +25,7 @@ class _DashboardScreenState extends State<DashboardScreen>
   static const headerAniInterval = Interval(.1, .3, curve: Curves.easeOut);
   late Animation<double> _headerScaleAnimation;
   AnimationController? _loadingController;
+
 
   @override
   void initState() {
@@ -79,8 +72,7 @@ class _DashboardScreenState extends State<DashboardScreen>
       onPressed: () => _goToLogin(context),
     );
     final title = Center(
-      child: Text('الصفحة الرئيسية',
-        style:GoogleFonts.tajawal(color: Colors.redAccent[100],fontSize: 24),
+      child: Text('الصفحة الرئيسية',style:GoogleFonts.tajawal(color: Colors.redAccent[100],fontSize: 24),
         textAlign: TextAlign.center,
         textDirection: TextDirection.rtl,),
     );
@@ -171,7 +163,7 @@ class _DashboardScreenState extends State<DashboardScreen>
           horizontal: 8.0,
           vertical: 8,
         ),
-        childAspectRatio: .9,
+        childAspectRatio: .8,
         // crossAxisSpacing: 5,
       crossAxisCount: 2,
       controller: new ScrollController(keepScrollOffset: false),
@@ -179,63 +171,93 @@ class _DashboardScreenState extends State<DashboardScreen>
       scrollDirection: Axis.vertical,
       // Generate 100 Widgets that display their index in the List
           children: [
-            _buildButton('بنك الاسئلة', Icon(FontAwesomeIcons.question,color: iconColor,size: iconSize),),
-            _buildButton('ملفات', Icon(FontAwesomeIcons.file,color: iconColor,size: iconSize),),
-            _buildButton('امتحانات', Icon(FontAwesomeIcons.tasks,color: iconColor,size: iconSize),),
-            _buildButton('فيدوهات', Icon(FontAwesomeIcons.fileVideo,color: iconColor,size: iconSize),),
-            _buildButton('رسائل', Icon(Icons.messenger,color: iconColor,size: iconSize),),
-            _buildButton('باركود', Icon(FontAwesomeIcons.qrcode,color: iconColor,size: iconSize),),
+            _buildButton('بنك الاسئلة',"https://www.edu2ksa.com/wp-content/uploads/2018/05/%D8%A8%D9%86%D9%83-%D8%A7%D9%84%D8%A7%D8%B3%D8%A6%D9%84%D8%A9-%D9%84%D9%85%D9%88%D8%A7%D8%AF-%D8%A7%D9%84%D8%B5%D9%81-%D8%A7%D9%84%D8%AB%D8%A7%D9%84%D8%AB-%D8%A7%D9%84%D8%A7%D8%A8%D8%AA%D8%AF%D8%A7%D8%A6%D9%8A-%D8%A7%D9%84%D9%81%D8%B5%D9%84-%D8%A7%D9%84%D8%AB%D8%A7%D9%86%D9%8A.jpg",),
+            _buildButton('ملفات', "https://www.europarl.europa.eu/resources/library/images/20190612PHT54317/20190612PHT54317-ml.jpg"),
+            _buildButton('امتحانات', "https://www.minia.edu.eg/images/nurse/nurse2020-08-042102381.jpg"),
+            _buildButton('فيدوهات',"https://img.freepik.com/free-photo/online-live-video-marketing-concept_12892-37.jpg?size=626&ext=jpg&ga=GA1.2.1937627297.1630454400"),
+            _buildButton('رسائل', "https://www.lifewire.com/thmb/IS4gxtmhvYTokbYCcm4ygOUBX50=/1920x1200/filters:fill(auto,1)/how-to-fix-the-unknown-message-not-found-on-iphone-error-849e332f4e9241db9cb80ef9ddb63e01.jpg",),
+            _buildButton('باركود', "https://me.kaspersky.com/content/ar-ae/images/repository/isc/2020/9910/a-guide-to-qr-codes-and-how-to-scan-qr-codes-1.jpg",),
 
           ],
     );
   }
 
 
-  Widget _buildButton(String text, Icon icon) {
+  Widget _buildButton(String text, String img) {
     return Padding(
-      padding: const EdgeInsets.all(4.0),
+      padding: const EdgeInsets.all(2.0),
       child: Container(
         decoration: BoxDecoration(
           color:  ColorResources.Beige,
           border: Border.all(
             color: ColorResources.Orange,
-            width: 2.0,
+            width: 1.0,
           ),
           borderRadius: BorderRadius.circular(10.0),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            icon,
-            SizedBox(height:12),
-            Text(text,style: GoogleFonts.tajawal(color: ColorResources.RedOrange,fontSize: 16),)
-          ],
+        child: Container(
+          color: Colors.white,
+          margin: new EdgeInsets.symmetric(
+            horizontal: 3.0,
+            vertical: 3.0,
+          ),
+          child: GestureDetector(
+            child: Padding(
+              padding: const EdgeInsets.all(0.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey.withOpacity(0.5),
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(1.0),
+                    color: Colors.white
+                ),
+
+                child: Stack(children: [
+                  Image.network(img,fit: BoxFit.cover,width: MediaQuery.of(context).size.width,height: MediaQuery.of(context).size.height,),
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey.withOpacity(0.5),
+                          width: 1.0,
+                        ),
+                        borderRadius: BorderRadius.circular(1.0),
+                        color: Colors.black38
+                    ),),
+                  Align(alignment:  Alignment.center,
+                    child: Text(
+                      text,
+                      maxLines: 1,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        shadows: <Shadow>[
+                          Shadow(
+                            offset: Offset(1.0, 1.0),
+                            blurRadius: 3.0,
+                            color: Color.fromARGB(255, 0, 0, 0),
+                          ),
+                        ],
+                        decoration: TextDecoration.none,
+                        fontSize: 18,
+                        fontFamily: "WorkSansMedium",
+                        color: Colors.white,
+                      ),
+                    ),)
+                ],),
+
+              ),
+            ),
+            onTap: () {
+
+            },
+
+          ),
         ),
       ),
     );
   }
 
-  // Widget _buildDebugButtons() {
-  //   const textStyle = TextStyle(fontSize: 12, color: Colors.white);
-  //
-  //   return Positioned(
-  //     bottom: 0,
-  //     right: 0,
-  //     child: Row(
-  //       children: <Widget>[
-  //         MaterialButton(
-  //           materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-  //           color: Colors.red,
-  //           onPressed: () => _loadingController!.value == 0
-  //               ? _loadingController!.forward()
-  //               : _loadingController!.reverse(),
-  //           child: Text('loading', style: textStyle),
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -279,4 +301,7 @@ class _DashboardScreenState extends State<DashboardScreen>
       ),
     );
   }
+
+
 }
+
